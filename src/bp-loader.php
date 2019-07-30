25be758c8ad1ff5ed23965233d9ef500a713fce8
Deepak Gupta <raftaar1191@gmail.com>
2019-07-30 12:29:40 +0530

    T:1150 prevent running the platform plugin #25

--- src/bp-loader.php
+++ src/bp-loader.php
@@ -66,6 +66,7 @@ if ( in_array( $bb_plugin_file, $bp_plugins ) ) {
 $bp_incompatible_plugins_list = array(
 	'buddypress-global-search/buddypress-global-search.php' => __( 'The BuddyBoss Platform can\'t work while BuddyPress Global Search plugin is active. Global Search functionality is built into the platform. Please deactivate BuddyPress Global Search first, if you wish to activate BuddyBoss Platform.', 'buddyboss' ),
 	'buddypress-followers/loader.php'                       => __( 'The BuddyBoss Platform can\'t work while BuddyPress Follow plugin is active. Follow functionality is built into the platform. Please deactivate BuddyPress Follow first, if you wish to activate BuddyBoss Platform.', 'buddyboss' ),
+	'bp-activity-shortcode/bp-activity-as-shortcode.php'    => __( 'The BuddyBoss Platform can\'t work while BuddyPress Activity ShortCode is active. Follow functionality is built into the platform. Please deactivate BuddyPress Activity ShortCode first, if you wish to activate BuddyBoss Platform.', 'buddyboss' ),
 );
 
 foreach ( $bp_incompatible_plugins_list as $incompatible_plugin => $error_message ) {
